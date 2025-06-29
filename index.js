2e06214c686347eb1eb05ee6af3a60e72be14aa4
Angelo Evangelista <angeloevan.ane@gmail.com>
2025-06-28 21:05:11 -0300

    feat: add initial code

---
+++ index.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import clipboardy from "clipboardy";
+
+async function getAWSCredentials() {
+  try {
+    const { data: credentials } = await axios.get(
+      process.env.AWS_CONTAINER_CREDENTIALS_FULL_URI,
+      {
+        headers: {
+          Authorization: process.env.AWS_CONTAINER_AUTHORIZATION_TOKEN,
+        },
+      }
+    );
+
+    const variablesToExport = {
+      AWS_ACCESS_KEY_ID: credentials.AccessKeyId,
+      AWS_SECRET_ACCESS_KEY: credentials.SecretAccessKey,
+      AWS_SESSION_TOKEN: credentials.Token,
+    };
+
+    const exportString = Object.keys(variablesToExport)
+      .reduce(
+        (acc, current) => [
+          ...acc,
+          `export ${current}=${variablesToExport[current]}`,
+        ],
+        []
+      )
+      .join("; ");
+
+    clipboardy.writeSync(exportString);
+
+    console.log(exportString);
+  } catch (error) {
+    console.error("Error fetching AWS credentials:", error.message);
+  }
+}
+
+getAWSCredentials();
